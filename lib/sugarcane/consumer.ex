79a207477f0a5877b6a22077ab303d6e98ffd061
Yslan Ramos <yslan3000@gmail.com>
2021-05-17 04:12:51 -0300

    refactor message event handler

--- lib/sugarcane/consumer.ex
+++ lib/sugarcane/consumer.ex
@@ -1,12 +1,16 @@
 defmodule Sugarcane.Consumer do
   use Nostrum.Consumer
-  alias Sugarcane.Context
+
+  alias Nostrum.Cache.Me
+  alias Nostrum.Struct.User
+  alias Sugarcane.{Context, Commands, Utils}
   alias Sugarcane.Commands
-  
+
+
   def start_link do
-    Consumer.start_link __MODULE__
+    Consumer.start_link(__MODULE__)
   end
-  
+
   def get_commands do
     [
       Commands.Ping,
@@ -17,34 +21,49 @@ defmodule Sugarcane.Consumer do
       Commands.Help
     ]
   end
-  
+
   def handle_event({:MESSAGE_CREATE, msg, ws_state}) when is_integer(msg.guild_id) and msg.author.bot != true and msg.author.id != 779748651035131945 do
-    Sugarcane.Metrics.ProcessedMessageInstrumenter.inc()
     guild = Sugarcane.Schemas.Guilds.get(Integer.to_string(msg.guild_id))
-    pattern = :binary.compile_pattern([guild.prefix, "sugarcane,", "<@779748651035131945>"])
-    
-    if String.starts_with?(msg.content, pattern) do
-      particles = msg.content
-      |> String.split(" ", trim: true)
 
-      [command_name | args] = particles
-      command_name = String.replace(String.downcase(command_name), pattern, "")
-      |> String.trim()
+    %User{id: bot_id, username: bot_username} = Me.get()
+    prefixes = [guild.prefix, "#{bot_username},", to_string(bot_id)]
 
-      ctx = %Context{msg: msg, args: args, guild_data: guild, ws_state: ws_state}
-      cmd = get_commands()
-      |> Enum.filter(fn(c) -> c.name == command_name or Enum.member?(c.aliases, command_name) end)
-      |> List.first()
-      
+    if String.contains?(msg.content, prefixes) do
+      [command_name | args] =
+        msg.content
+        |> Utils.remove_first_char()
+        |> String.split(" ", trim: true)
+
+
+      cmd = fetch_command(command_name)
+      IO.inspect cmd
       if cmd != nil do
+        ctx = %Context{
+          msg: msg,
+          args: args,
+          guild_data: guild,
+          ws_state: ws_state
+        }
+
         Sugarcane.Metrics.CommandInstrumenter.inc()
         Nostrum.Api.start_typing(msg.channel_id)
         cmd.run(ctx)
       end
     end
   end
-  
+
+  # Default event handler, if you don't include this, your consumer WILL crash if
+  # you don't have a method definition for each event type.
   def handle_event(_event) do
     :noop
+  end
+
+  defp fetch_command(name) when is_binary(name) do
+    name = String.downcase(name)
+
+    Enum.find(get_commands(),
+     fn cmd ->
+      cmd.name() === name or Enum.member?(cmd.aliases(), name)
+     end)
   end
 end
